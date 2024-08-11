5e0261481f69b71c3844244798a00f9826c5bde6
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-08-11 08:03:45 -0300

    feat: config active profile and mysql

--- src/test/java/com/products/ProductIT.java
+++ src/test/java/com/products/ProductIT.java
@@ -2,10 +2,12 @@ package com.products;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 //
 // Tests end-to-end
 //
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductIT {
 
