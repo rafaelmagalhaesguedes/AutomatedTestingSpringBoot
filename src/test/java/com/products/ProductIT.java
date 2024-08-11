ec6572dca9068713e4565af54fb3b1a941406ec7
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-08-11 07:58:45 -0300

    feat: config server to tests end-to-end

---
+++ src/test/java/com/products/ProductIT.java
@@ -0,0 +1,16 @@
+package com.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//
+// Tests end-to-end
+//
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProductIT {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
